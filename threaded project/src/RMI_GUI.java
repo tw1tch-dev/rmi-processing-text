3a8490f3a2755061c445d84ed0e9366e7ec2e871
tw1tch-dev <magdya595@yahoo.com>
2023-05-20 00:48:33 +0300

    added GUI comments
    explaining some stuff on the GUI java file, what some of the functions do.. etc

--- threaded project/src/RMI_GUI.java
+++ threaded project/src/RMI_GUI.java
@@ -12,26 +12,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class RMI_GUI extends JFrame {
+public class RMI_GUI extends JFrame { // a class that is a frame window that can be used in GUI apps
     private JTextArea outputTxt;
 
+    // used the javax.swing package, it is for creating and managing GUI stuff life windows, buttons.. etc
     public RMI_GUI() {
+        // set window title and exit operation
         setTitle("RMI Client GUI");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+        setLayout(new BorderLayout()); // set layout
 
+        // create jtextarea object and set it uneditable
         outputTxt = new JTextArea();
         outputTxt.setEditable(false);
+
+        // create jscrollpane object and add jtextarea to it
         JScrollPane scrollPane = new JScrollPane(outputTxt);
         add(scrollPane, BorderLayout.CENTER);
 
-        JButton analyzeButton = new JButton("Calculate");
-        analyzeButton.addActionListener(new ActionListener() {
+        // create the button for calculation
+        JButton calcButton = new JButton("Calculate");
+        calcButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 calculateBtn();
             }
         });
 
+        // create button to clear window
         JButton clearButton = new JButton("Clear");
         clearButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -39,23 +46,29 @@ public class RMI_GUI extends JFrame {
             }
         });
 
+        // create panel to have the buttons inside it
         JPanel buttonPanel = new JPanel();
-        buttonPanel.add(analyzeButton);
+        buttonPanel.add(calcButton);
         buttonPanel.add(clearButton);
-        add(buttonPanel, BorderLayout.SOUTH);
+        add(buttonPanel, BorderLayout.SOUTH); // set panel location
 
+        // adjusting the size of window
         setPreferredSize(new Dimension(500, 400));
-
         pack();
+
+        // center the window in the screen
         setLocationRelativeTo(null);
     }
 
     private void calculateBtn() {
         try {
+            // gets the reference of RMI registry at port 1099
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+            // searches for a remote object under name "calculation"
             process_txt computeAll = (process_txt) registry.lookup("calculation");
 
             List<String> words = new ArrayList<>();
+            // reading the file
             BufferedReader file = new BufferedReader(new FileReader("C:/Users/Administrator/Downloads/threaded project/src/words.txt"));
             String word;
             while ((word = file.readLine()) != null) {
@@ -67,6 +80,7 @@ public class RMI_GUI extends JFrame {
             file.close();
             StringBuffer x = new StringBuffer();
 
+            // concatenate the words into one string
             for(String word1 : words) {
                 x.append(word1).append(" ");
             }
@@ -143,6 +157,7 @@ public class RMI_GUI extends JFrame {
             repeatedWordsThread.start();
             wordCountsThread.start();
 
+            // wait for each thread to finish to calculate the finish time
             try {
                 countThread.join();
                 longestThread.join();
@@ -154,7 +169,7 @@ public class RMI_GUI extends JFrame {
             }
             long finished = System.currentTimeMillis();
 
-            long time = finished - started;
+            long time = finished - started; // total time
 
             printTxt("Program execution time: " + time + " milliseconds");
             printTxt("____________________________________");
@@ -164,17 +179,21 @@ public class RMI_GUI extends JFrame {
         }
     }
 
+    // clears the windows of the GUI
     private void clearWindow() {
         outputTxt.setText("");
     }
 
+    // prints the text into the GUI window
     private synchronized void printTxt(String text) {
         outputTxt.append(text + "\n");
     }
 
     public static void main(String[] args) {
+        // executing GUI setup on event thread, ensures the setup is executed
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                // create instance of the GUI and set it to be visible
                 RMI_GUI gui = new RMI_GUI();
                 gui.setVisible(true);
             }
